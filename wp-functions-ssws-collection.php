169adaaa9745f3135324d0805189798277d21396
Giorgio Riccardi <griccardi@my.bcit.ca>
2019-08-27 18:35:13 -0700

    Export API Data to JSON function/plugin

--- wp-functions-ssws-collection.php
+++ wp-functions-ssws-collection.php
@@ -1980,3 +1980,44 @@ function ssws_update_draft_posts_to_publish()
 // note that this function will not publish custom fields 
 // unless the double click publish button is disabled in the Gutenberg options
 // by default Gutenberg will ask to re-click the publish button to make sure you checked everything twice (rather annoying!)
+
+/********************************************************/
+/*
+Plugin Name: Export API Data to JSON
+Author: SSWS
+Author URI: https://stackoverflow.com/users/2517011/giorgio25b
+Description: Every time you save,update or delete a post, all the published post are getting saved in a JSON file in the uploads directory. Have in mind that by default it only exports "title - excerpt - author" , but you can add whatever else you want.
+ */
+/********************************************************/
+function export_posts_in_json()
+{
+    $args = array(
+        'post_type' => 'post',
+        'post_status' => 'publish',
+        'posts_per_page' => -1,
+    );
+    $query = new WP_Query($args);
+    $posts = array();
+    while ($query->have_posts()): $query->the_post();
+        $posts[] = array(
+            'title' => get_the_title(),
+            'excerpt' => get_the_excerpt(),
+            'author' => get_the_author(),
+        );
+    endwhile;
+    wp_reset_query();
+    $data = json_encode($posts);
+    $upload_dir = wp_get_upload_dir(); // set to save in the /wp-content/uploads folder
+    $file_name = date('Y-m-d') . '.json';
+    $save_path = $upload_dir['basedir'] . '/' . $file_name;
+
+    $f = fopen($save_path, "w"); //if json file doesn't gets saved, comment this and uncomment the one below
+    //$f = @fopen( $save_path , "w" ) or die(print_r(error_get_last(),true)); //if json file doesn't gets saved, uncomment this to check for errors
+    fwrite($f, $data);
+    fclose($f);
+
+}
+add_action('save_post', 'export_posts_in_json');
+// https://wordpress.stackexchange.com/questions/232708/export-all-post-from-database-to-json-only-when-the-database-gets-updated
+// my answer here: 
+// https://stackoverflow.com/questions/43787499/wordpress-rest-api-write-to-json-file
