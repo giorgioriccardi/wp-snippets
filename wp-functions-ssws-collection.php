c22b6a31687c25d503478b09ed40e4d5251bd733
Giorgio Riccardi <griccardi@my.bcit.ca>
2019-10-15 19:08:36 -0700

    Change dashboard Posts label to News

--- wp-functions-ssws-collection.php
+++ wp-functions-ssws-collection.php
@@ -2081,3 +2081,25 @@ function export_posts_to_json()
     file_put_contents('posts_export.json', $json);
 }
 add_action('wp_loaded', 'export_posts_to_json');
+
+/********************************************************/
+// Change dashboard Posts label to News
+/********************************************************/
+add_action( 'init', 'ssws_change_post_object' );
+function ssws_change_post_object() {
+    $get_post_type = get_post_type_object('post');
+    $labels = $get_post_type->labels;
+        $labels->name = 'News';
+        $labels->singular_name = 'News';
+        $labels->add_new = 'Add News';
+        $labels->add_new_item = 'Add News';
+        $labels->edit_item = 'Edit News';
+        $labels->new_item = 'News';
+        $labels->view_item = 'View News';
+        $labels->search_items = 'Search News';
+        $labels->not_found = 'No News found';
+        $labels->not_found_in_trash = 'No News found in Trash';
+        $labels->all_items = 'All News';
+        $labels->menu_name = 'News';
+        $labels->name_admin_bar = 'News';
+}
