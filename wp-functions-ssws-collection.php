6b4d704ea06abbbc03c5d25cb8bc5bf6c7580c4e
Giorgio Riccardi <giorgio.riccardi.ca@gmail.com>
2020-07-30 13:13:42 -0700

    add rel noopener for external links in new tab solution

--- wp-functions-ssws-collection.php
+++ wp-functions-ssws-collection.php
@@ -1740,6 +1740,7 @@ function openExternalLinksNewTab() {
         for (var i = 0, linksLength = links.length; i < linksLength; i++) {
             if (links[i].hostname != window.location.hostname) {
                 links[i].target = '_blank';
+                links[i].rel = 'noopener';
                 // console.log('ext-link');
             }
         }
